2e7dc7f276a472a560645831cbe47547d72cada3
Maria Paderina <mariapaderina79@gmail.com>
2019-05-04 17:32:44 -0700

    empty project

---
+++ swift-project/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  swift-project
+//
+//  Created by Maria Paderina on 2019-05-04.
+//  Copyright © 2019 MashaStudio. All rights reserved.
+//
+
+import Foundation
+
+
+print("Hello, World!")
+
